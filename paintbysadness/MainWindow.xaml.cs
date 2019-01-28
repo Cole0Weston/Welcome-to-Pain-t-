aa64fecae4ce370eb0dde9777ef8cb4ce13f9982
Cole Weston <colmwest@ut.utm.edu>
2019-01-28 13:49:30 -0600

    Add project files.

---
+++ paintbysadness/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace paintbysadness
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            happy_little_trees.DefaultDrawingAttributes.Color = Colors.Black;
+            happy_little_trees.DefaultDrawingAttributes.Height = 20;
+            happy_little_trees.DefaultDrawingAttributes.Width = 20;
+
+        }
+        //Color Toggle
+        private void RED_Click(object sender, RoutedEventArgs e)
+        {
+            happy_little_trees.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+        private void BLUE_Click(object sender, RoutedEventArgs e)
+        {
+            happy_little_trees.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+        private void GREEN_Click(object sender, RoutedEventArgs e)
+        {
+            happy_little_trees.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+        private void BLACK_Click(object sender, RoutedEventArgs e)
+        {
+            happy_little_trees.DefaultDrawingAttributes.Color = Colors.Black;
+        }
+        //Eraser Selection
+        private void ERASER_Click(object sender, RoutedEventArgs e)
+        {
+            happy_little_trees.DefaultDrawingAttributes.Color = Colors.White;
+        }
+        //Clear Button
+        private void CLEAR_Click(object sender, RoutedEventArgs e)
+        {
+            happy_little_trees.Strokes.Clear();
+        }
+        //Thickness Toggle
+        private void SMALL_Click(object sender, RoutedEventArgs e)
+        {
+            happy_little_trees.DefaultDrawingAttributes.Height = 20;
+            happy_little_trees.DefaultDrawingAttributes.Width = 20;
+        }
+        private void MEDIUM_Click(object sender, RoutedEventArgs e)
+        {
+            happy_little_trees.DefaultDrawingAttributes.Height = 30;
+            happy_little_trees.DefaultDrawingAttributes.Width = 30;
+        }
+        private void LARGE_Click(object sender, RoutedEventArgs e)
+        {
+            happy_little_trees.DefaultDrawingAttributes.Height = 40;
+            happy_little_trees.DefaultDrawingAttributes.Width = 40;
+        }
+    }
+
+}
+
---
+++ paintbysadness/README.txt
@@ -0,0 +1,12 @@
+﻿NAME: Cole Weston
+FILE: README.txt
+Purpose of Program:
+Create a simple paint application capable of swapping colors, changing thickness of pen, erasing, and clearing the canvas.
+Assumptions:
+User is not Picaso
+Design Decisions:
+Provided easy to navigate menu bar.
+Thickness Toggle is bottom right 3 buttons.
+Final Note:
+Full screen does not work, but may be implemented in future updates. "May."
+Designer is color blind.
